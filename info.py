3ee8228bb0ed2c79dedf3066d4d11f72c3a959fb
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-04-04 13:50:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Auto_Filters_Bot')
 API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('', '')
+API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 # Bot pics and stickers
